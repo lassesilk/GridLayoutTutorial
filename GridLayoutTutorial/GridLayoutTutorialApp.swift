61e1e27d6394e4a4318a7867e2646811ca86fe66
lassesilk <lassesilkoset@gmail.com>
2022-08-31 08:42:26 +0200

    Initial Commit

---
+++ GridLayoutTutorial/GridLayoutTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridLayoutTutorialApp.swift
+//  GridLayoutTutorial
+//
+//  Created by Lasse Silkoset on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GridLayoutTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
